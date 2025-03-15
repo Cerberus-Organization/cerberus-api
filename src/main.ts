83b36432b675a2b5a617c45b1929ee1fdb13ab30
luis_thiago <lthiagofarias@gmail.com>
2025-03-15 14:56:50 -0300

    feat: swagger setup

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // **Swagger Setup**
+  const config = new DocumentBuilder()
+    .setTitle('Cerberus API')
+    .setDescription('https://github.com/Cerberus-Organization')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+  // **Swagger Setup**
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
